66d06b6bce31144f3c76784e7007b338828699f6
HackerMan237 <93648451+LekeneCedric@users.noreply.github.com>
2024-04-12 23:43:50 +0100

    [#INIT] - setup nodejs & typescript configurations

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import 'dotenv/config'
+
+const app = express();
+const port = process.env.PORT || 8000 || 4000;
+
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`[SERVER]: started at ${port}`))
