7c93d581c143c13151ba022f1dc5ed90ee536037
HackerMan237 <93648451+LekeneCedric@users.noreply.github.com>
2024-04-22 03:55:36 +0100

    - pending start auth

--- src/Auth/infrastructure/Http/controllers/SignInController.ts
+++ src/Auth/infrastructure/Http/controllers/SignInController.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from 'express';
+import SignInCommandFactory from '../../Factories/SignInCommandFactory';
 
+interface ISignInResponse {
+
+}
 const SignInController = async (req: Request, res: Response) => {
+	const httpResponse = {} as ISignInResponse
 
-	res.status(200).send({ 'message': 'sign_in' })
+	try {
+		const command = SignInCommandFactory.buildFromRequest(req);
+	} catch() {
+
+	}
+
+	res.status(200).send(httpResponse);
 };
 
 export default SignInController;
